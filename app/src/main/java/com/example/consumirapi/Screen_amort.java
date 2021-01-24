a194c334fe170321ba0b342388d32ca7d1a44e04
JulioMR17 <tecnotic4d@gmail.com>
2021-01-24 14:42:06 +0100

    Programando DropDwon de la Screen_amort - Julio

--- app/src/main/java/com/example/consumirapi/Screen_amort.java
+++ app/src/main/java/com/example/consumirapi/Screen_amort.java
@@ -3,50 +3,26 @@ package com.example.consumirapi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-import android.widget.Toast;
 
-public class Screen_amort extends AppCompatActivity
-{
+public class Screen_amort extends AppCompatActivity {
 
-    Spinner dropdown;
+    Spinner spinner_select;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen_amort);
 
-        dropdown = findViewById(R.id.spinner);
+        spinner_select = findViewById(R.id.spinner);
 
 
         String[] opciones = {"Prestamo hipotecario", "Prestamo personal"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.spinner_item, opciones);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout., opciones);
+        spinner_select.setAdapter(adapter);
 
-        dropdown.setAdapter(adapter);
+
     }
-
-    public void dropDown(View v)
-    {
-
-        String seleccion = dropdown.getSelectedItem().toString();
-
-        switch (seleccion)
-        {
-            case "Prestamo hipotecario":
-                Toast.makeText(this, "Prestamo hipotecario", Toast.LENGTH_SHORT).show();
-                break;
-            case "Prestamo personal":
-                Toast.makeText(this, "Prestamo personal", Toast.LENGTH_SHORT).show();
-                break;
-
-        }
-    }
-
-
-
-
 }
