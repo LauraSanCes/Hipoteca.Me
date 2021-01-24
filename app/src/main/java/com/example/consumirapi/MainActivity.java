3dc3634a666d570c0089b7ed0741ee0a18f73b89
JulioMR17 <tecnotic4d@gmail.com>
2021-01-24 12:58:19 +0100

    Botones EXIT y CREDITOS - Julio

--- app/src/main/java/com/example/consumirapi/MainActivity.java
+++ app/src/main/java/com/example/consumirapi/MainActivity.java
@@ -1,14 +1,56 @@
 package com.example.consumirapi;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    Button amort, conversor, calculadora, exit, creditos;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        amort = findViewById(R.id.btn_amort);
+        conversor = findViewById(R.id.btn_conver);
+        calculadora = findViewById(R.id.btn_calc);
+        exit = findViewById(R.id.btn_exit);
+        creditos = findViewById(R.id.btn_cred);
+    }
+
+
+   public void onClickAmort(View v)
+   {
+
+   }
+
+    public void onClickConver(View v)
+    {
+
+    }
+
+    public void onClickCalc(View v)
+    {
+
+    }
+
+    public void onClickExit(View v)
+    {
+        System.exit(0);
+    }
+
+    public void onClickCredit(View v)
+    {
+        Intent i = new Intent(this, Screen_credits.class);
+
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/example/consumirapi/Screen_credits.java
@@ -0,0 +1,14 @@
+package com.example.consumirapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Screen_credits extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_credits);
+    }
+}
