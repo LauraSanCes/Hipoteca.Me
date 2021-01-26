125a1fdfbfb3b88b4d856c59d20511eff9aa6816
JulioMR17 <tecnotic4d@gmail.com>
2021-01-26 01:18:56 +0100

    Programando selección DropDownList - Julio

--- app/src/main/java/com/example/consumirapi/Screen_amort.java
+++ app/src/main/java/com/example/consumirapi/Screen_amort.java
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Screen_amort extends AppCompatActivity {
 
     Spinner spinner_select;
+    TextView txt_drop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,32 +21,40 @@ public class Screen_amort extends AppCompatActivity {
         setContentView(R.layout.activity_screen_amort);
 
         spinner_select = findViewById(R.id.spinner);
+        txt_drop = findViewById(R.id.txtv);
 
 
-        String[] opciones = {"Prestamo hipotecario", "Prestamo personal"};
+        final String[] opciones = {"Prestamo hipotecario", "Prestamo personal"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.spinner_item, opciones);
 
         spinner_select.setAdapter(adapter);
 
-    }
+        spinner_select.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // get selected item and assign to textview
+                // ex: textView.setText(spinner.getSelectedItem().toString());
 
-    public void spinnerClick(View view)
-    {
-        String seleccion = spinner_select.getSelectedItem().toString();
+               if (spinner_select.getSelectedItemPosition() == 0)
+               {
+                   txt_drop.setText("Prestamo hipotecario");
+               } else {
+                   txt_drop.setText("Prestamo personal");
+               }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // handle if you'd like to
+            }
+        });
 
 
-        switch (seleccion)
-        {
-            case "Prestamo hipotecario":
-                Toast.makeText(this, "Prestamo hipotecario", Toast.LENGTH_SHORT).show();
-                break;
-            case "Prestamo personal":
-                Toast.makeText(this, "Prestamo personal", Toast.LENGTH_SHORT).show();
-                break;
-        }
-        //tv1.setText(seleccion);
-    }
+    } //End onCreate
 
+//TODO terminar diseño pantalla y programar componentes
+//TODO añadir los rotulos recortados
 
 
 }
