b9dff224ca8125b6be0e89d8cc69c232e4d4ba15
JulioMR17 <tecnotic4d@gmail.com>
2021-01-28 14:16:45 +0100

    Programando componentes Pantalla Amort - Julio

--- app/src/main/java/com/example/consumirapi/Screen_amort.java
+++ app/src/main/java/com/example/consumirapi/Screen_amort.java
@@ -3,9 +3,11 @@ package com.example.consumirapi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,7 +15,8 @@ import android.widget.Toast;
 public class Screen_amort extends AppCompatActivity {
 
     Spinner spinner_select;
-    TextView txt_drop;
+    TextView txt_tin, txt_tae;
+    EditText ed_txt_cant, ed_txt_cuot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +24,10 @@ public class Screen_amort extends AppCompatActivity {
         setContentView(R.layout.activity_screen_amort);
 
         spinner_select = findViewById(R.id.spinner);
-        //txt_drop = findViewById(R.id.txtv);
+        txt_tin = findViewById(R.id.textView_tin);
+        txt_tae = findViewById(R.id.textView_tae);
+        ed_txt_cant = findViewById(R.id.editText_cant);
+        ed_txt_cuot = findViewById(R.id.editText_cuot);
 
 
         final String[] opciones = {"Prestamo hipotecario", "Prestamo personal"};
@@ -35,12 +41,14 @@ public class Screen_amort extends AppCompatActivity {
                 // get selected item and assign to textview
                 // ex: textView.setText(spinner.getSelectedItem().toString());
 
-             /*  if (spinner_select.getSelectedItemPosition() == 0)
+               if (spinner_select.getSelectedItemPosition() == 0)
                {
-                   txt_drop.setText("Prestamo hipotecario");
+                   txt_tin.setText("3%");
+                   txt_tae.setText("5%");
                } else {
-                   txt_drop.setText("Prestamo personal");
-               }*/
+                   txt_tin.setText("2%");
+                   txt_tae.setText("4%");
+               }
 
             }
 
@@ -53,7 +61,25 @@ public class Screen_amort extends AppCompatActivity {
 
     } //End onCreate
 
-//TODO terminar diseño pantalla y programar componentes
+public void calcular(View v)
+{
+
+    String cantidad_txt = ed_txt_cant.getText().toString();
+
+    int cantidad = Integer.parseInt(cantidad_txt);
+
+   String cuotas_txt = ed_txt_cuot.getText().toString();
+
+   int cuotas = Integer.parseInt(cuotas_txt);
+
+
+   if((cantidad < 0 || cantidad > 400000) || (cuotas < 0 || cuotas > 240))
+   {
+       Toast.makeText(this, "ERROR", Toast.LENGTH_SHORT).show();
+   }
+
+
+}
 
 
 
