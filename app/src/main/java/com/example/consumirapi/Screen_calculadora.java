324d7d7af907c9169535c5d727c1d41210c4fdc8
LauraSanCes <77784381+LauraSanCes@users.noreply.github.com>
2021-02-11 13:40:37 +0100

    [CODING] calculadora- laura

--- app/src/main/java/com/example/consumirapi/Screen_calculadora.java
+++ app/src/main/java/com/example/consumirapi/Screen_calculadora.java
@@ -1,14 +1,49 @@
 package com.example.consumirapi;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class Screen_calculadora extends AppCompatActivity {
+
+public class Screen_calculadora extends AppCompatActivity
+{
+    Button btnCero, btnUno, btnDos, btnTres, btnCuatro, btnCinco, btnSeis, btnSiete, btnOcho, btnNueve,
+            btnSuma, btnResta, btnMultiplica, btnDivide, btnClean, btnBorrar, btnPunto, btnIgual;
+
+    TextView Resultado;
+
+    double resultado;
+
+    String operador = "";
+    String mostrar , reserva;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen_calculadora);
+
+        btnCero = findViewById(R.id.Cero);
+        btnUno = findViewById(R.id.Uno);
+        btnDos = findViewById(R.id.Dos);
+        btnTres = findViewById(R.id.Tres);
+        btnCuatro = findViewById(R.id.Cuatro);
+        btnCinco = findViewById(R.id.Cinco);
+        btnSeis = findViewById(R.id.Seis);
+        btnSiete = findViewById(R.id.Siete);
+        btnOcho = findViewById(R.id.Ocho);
+        btnNueve = findViewById(R.id.Nueve);
+        btnSuma = findViewById(R.id.Suma);
+        btnResta = findViewById(R.id.Resta);
+
+
+
+
+
+
     }
 }
